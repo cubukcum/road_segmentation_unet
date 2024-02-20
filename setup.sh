5a39d682e1336236425d2f09f9d25c10e3f844ff
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-02-20 05:04:35 -0600

    provided link to download the model

--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@ fi
 # Install Python dependencies
 python3 -m pip install pip --upgrade
 python3 -m pip install -r requirements.txt
+
+modelUrl = "https://drive.google.com/file/d/11yaV9vo7FeI5svtL_mcV0F_I8L39FZf9/view?usp=drive_link"
+
+# Download TF Lite model
+FILE=${DATA_DIR}/model.tflite
+if [ ! -f "$FILE" ]; then
+  curl \
+    -L "$modelUrl" -o ${FILE}
+fi
